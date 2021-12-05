d2a7c1f0e91dd52b8a51ff8201b5f9f6eda0e2f6
Aditi-1399 <87570235+Aditi-1399@users.noreply.github.com>
2021-12-05 16:20:17 +0530

    Database creation

--- Spring MVC Lab.sql
+++ Spring MVC Lab.sql
@@ -4,4 +4,3 @@ create table student(studentId int primary key, studentName varchar(50), departm
 insert into student values(1,"Aditi","Mechanical","India");
 insert into student values(2,"Sneha","Electrical","Japan");
 select * from student;
-drop table student;
